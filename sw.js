7931a7a9d655d870b7862894f9d9fd728286567e
kharratmarouaninfo <kharrat.marouan.info@gmail.com>
2026-06-08 11:32:57 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'ytplayer-v1';
+const ASSETS = [
+  '/yt-player/',
+  '/yt-player/index.html',
+  '/yt-player/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
